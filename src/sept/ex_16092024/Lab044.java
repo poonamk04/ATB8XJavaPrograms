556be3994233933f8833571e8d94a73597a64a5e
poonamk04 <poonamkshirsagar04@gmail.com>
2024-09-28 16:45:37 +0530

    add all the files - 28th sept

---
+++ src/sept/ex_16092024/Lab044.java
@@ -0,0 +1,10 @@
+package sept.ex_16092024;
+
+public class Lab044 {
+    public static void main(String[] args) {
+        //new operator
+        String s1 = new String("Poonam");
+        //instanceof operator
+        System.out.println(s1 instanceof String);
+    }
+}
---
+++ src/sept/ex_16092024/Lab045.java
@@ -0,0 +1,22 @@
+package sept.ex_16092024;
+
+public class Lab045 {
+    public static void main(String[] args) {
+        short s = 10;
+        char c = 'A'; //65
+        int ss = s+c;
+        System.out.println(ss);
+    }
+}
+ /* difference between = and ==
+ = used to assign the value
+ == used to compare two values
+
+ byte b = 10; ---- 1byte
+ long l = 10l; ---- 8 byte
+ so total 9 byte
+
+ byte b = 10;
+ byte c = 10;
+ so b+c = int 20
+  */
