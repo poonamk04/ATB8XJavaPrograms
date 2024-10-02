cfa218d957df43a50dc96253f9c681b82156f3a0
poonamk04 <poonamkshirsagar04@gmail.com>
2024-10-02 19:23:31 +0530

    add all the files - 2st oct

--- src/Task/Task_25092024_3.java
+++ src/Task/Task_25092024_3.java
@@ -4,9 +4,18 @@ public class Task_25092024_3 {
     public static void main(String[] args) {
         //Prime no
         int no = 100;
-        for (int i = 1;i<=100;i++)
+        System.out.println("Prime Numbers between 1---->100 are :");
+        for(int i=1; i<=no; i++)
         {
-            if ()
+            int count =0;
+            for(int j=1;j<=no; j++) {
+                if(i%j==0) {
+                    count++;
+                }
+            }
+            if(count==2) {
+                System.out.print(i + " ");
+            }
         }
     }
 }
