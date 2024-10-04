460762154f229bedbfd25f1b6bcdad215d4bb313
poonamk04 <poonamkshirsagar04@gmail.com>
2024-10-04 18:38:04 +0530

    add all the files - 4rd oct

---
+++ src/Task/Task_ATBStudent.java
@@ -0,0 +1,24 @@
+package Task;
+
+public class Task_ATBStudent {
+    public static void main(String[] args) {
+        int name;
+        int age;
+        long phoneno;
+        int sid;
+
+        //void eat();
+        //void sleep();
+        //void study();
+
+        Task_ATBStudent stu1 = new Task_ATBStudent();
+        Task_ATBStudent stu2 = new Task_ATBStudent();
+        Task_ATBStudent stu3 = new Task_ATBStudent();
+        Task_ATBStudent stu4 = new Task_ATBStudent();
+        Task_ATBStudent stu5 = new Task_ATBStudent();
+
+
+
+
+    }
+}
